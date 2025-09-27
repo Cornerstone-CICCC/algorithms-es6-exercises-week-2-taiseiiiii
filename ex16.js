8e0a304600af1264c97e5fcac00d360010bfb408
taiseiiiii <isemiya.0509@gmail.com>
2025-09-26 20:24:06 -0700

    complete ex16

--- ex16.js
+++ ex16.js
@@ -16,7 +16,120 @@ For more information on casing styles, read Wikipedia's Special Case Styles for 
 */
 
 const makeCaze = function (input, caze) {
-  // Put your solution here
+  const words = input.split(" ");
+  const cases = Array.isArray(caze) ? caze : [caze];
+  let res = input;
+  for (let i = 0; i < cases.length; i++) {
+    const target = cases[i];
+
+    if (target === "camel") {
+      res = camel(words);
+    }
+    if (target === "pascal") {
+      res = pascal(words);
+    }
+    if (target === "snake") {
+      res = snake(words);
+    }
+    if (target === "kebab") {
+      res = kebab(words);
+    }
+    if (target === "title") {
+      res = title(words);
+    }
+    if (target === "vowel") {
+      res = vowel(res);
+    }
+    if (target === "consonant") {
+      res = consonant(res);
+    }
+    if (target === "upper") {
+      res = upper(res);
+    }
+    if (target === "lower") {
+      res = lower(res);
+    }
+  }
+
+  return res;
+};
+
+const camel = (input) => {
+  return input.reduce(
+    (res, cur, index) =>
+      index === 0
+        ? (res += cur.toLowerCase())
+        : (res += cur[0].toUpperCase() + cur.slice(1).toLowerCase()),
+    ""
+  );
+};
+
+const pascal = (input) => {
+  return input.reduce(
+    (res, cur) => (res += cur[0].toUpperCase() + cur.slice(1).toLowerCase()),
+    ""
+  );
+};
+
+const snake = (input) => {
+  return input.reduce(
+    (res, cur, index) =>
+      res + cur.toLowerCase() + (index < input.length - 1 ? "_" : ""),
+    ""
+  );
+};
+
+const kebab = (input) => {
+  return input.reduce(
+    (res, cur, index) =>
+      res + cur.toLowerCase() + (index < input.length - 1 ? "-" : ""),
+    ""
+  );
+};
+
+const title = (input) => {
+  return input.reduce(
+    (res, cur, index) =>
+      res +
+      cur[0].toUpperCase() +
+      cur.slice(1).toLowerCase() +
+      (index < input.length - 1 ? " " : ""),
+    ""
+  );
+};
+
+const vowelArr = ["a", "i", "u", "e", "o"];
+
+const vowel = (input) => {
+  return input
+    .split("")
+    .reduce(
+      (res, curr) =>
+        res +
+        (vowelArr.includes(curr.toLowerCase()) ? curr.toUpperCase() : curr),
+      ""
+    );
+};
+
+const consonant = (input) => {
+  return input
+    .split("")
+    .reduce(
+      (res, curr) =>
+        res +
+        (vowelArr.includes(curr.toLowerCase())
+          ? curr.toLowerCase()
+          : curr.toUpperCase()),
+      ""
+    );
+};
+
+const upper = (input) => {
+  return input.toUpperCase();
+};
+
+const lower = (input) => {
+  return input.toLowerCase();
 };
 
 console.log(makeCaze("this is a string", "camel")); // thisIsAString

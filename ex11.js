92b9b503206157b193c633a607d365799ddd43c4
taiseiiiii <isemiya.0509@gmail.com>
2025-09-27 00:13:29 -0700

    complete ex11

--- ex11.js
+++ ex11.js
@@ -28,25 +28,36 @@ Implement the functions one by one. The example inputs and outputs below will he
 const PI = 3.14159;
 
 const sphereVolume = function (radius) {
-  // Code here!
+  return (4 / 3) * PI * radius ** 3;
 };
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); //true
 
 const coneVolume = function (radius, height) {
-  // And here!
+  return (1 / 3) * PI * radius ** 2 * height;
 };
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); //true
 
 const prismVolume = function (height, width, depth) {
-  // Probably here too!
+  return height * width * depth;
 };
 
 console.log(prismVolume(3, 4, 5) === 60); //true
 
 const totalVolume = function (solids) {
-  // Code here? Yup!
+  return solids.reduce((total, solid) => {
+    switch (solid.type) {
+      case "sphere":
+        return total + sphereVolume(solid.radius);
+      case "cone":
+        return total + coneVolume(solid.radius, solid.height);
+      case "prism":
+        return total + prismVolume(solid.height, solid.width, solid.depth);
+      default:
+        return total;
+    }
+  }, 0);
 };
 
 const largeSphere = {
